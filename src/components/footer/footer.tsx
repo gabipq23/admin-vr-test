f46ce1c1d64796e900b4e51a85bfb4df32487c90
Gabriela Quintaes <gabrielapquintaes@gmail.com>
2026-03-11 10:29:42 -0300

    feat: update project name to 'admin-vr-test' in package files and add logo to footer

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,8 +1,11 @@
 function Footer() {
   return (
-    <footer className="flex items-center justify-center text-sm h-14  lg:h-10 bg-[#c5c5c5]">
+    <footer className="flex items-center justify-between text-sm h-14 px-6 md:px-10 lg:px-14 lg:h-10 bg-[#c5c5c5]">
       <p className="text-[#008a1e] text-center text-[13px] ">
-        © 2026 VR – CNPJ 02.535.864/0001-33 – Todos os direitos reservados
+        © 2026 – Todos os direitos reservados
+      </p>
+      <p className="text-[#008a1e] text-center text-[13px] flex items-center gap-1">
+        Powered by   <img src="\assets\megalead.png" className="h-7"></img>
       </p>
     </footer>
   );
